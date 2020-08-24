0717326f57693bd5e98dd12ac9be9dcf8085ff4b
eugenek45 <eugenekiprotich0@gmail.com>
2020-08-24 11:57:06 +0300

    crested files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
+// import { HighlightDirective } from './highlight.directive';
+import { DetailsComponent } from './details/details.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    QuoteComponent,
+    // HighlightDirective,
+    DetailsComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    // MDBBootstrapModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote("qoutes", "Eleanor Roosevelt", "Learn from the mistakes of others. You can't live long enough to make them all yourself."),
+    new Quote("Life", "W.E.B. Du Bois", "The most important thing to remember is this: to be ready at any moment to give up what you are for what you might become ")
+  ]
+    
+
+  addQuote(chosenQuote) {
+    this.quotes.push(chosenQuote)
+  }
+
+  upvote(i) {
+    this.quotes[i].upvotes ++;
+  }
+  downvote(i) {
+    this.quotes[i].downvotes  ++;
+  }
+  delQuote(i) {
+    this.quotes.splice(i, 1)
+  }
+  preNum:number
+  lastNum:number
+  counter:number
+
+  highestUpvote(){
+    this.preNum = 0
+    this.lastNum = 0
+
+    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].upvotes;
+      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+    }
+    return  this.preNum
+  }
+
+
+
+constructor() { }
+
+ngOnInit() {
+}
+
+}
